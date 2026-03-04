0506808386cde7ca428fef32d91d086f606ef9d6
MaximeCode <contact.mbaude@gmail.com>
2026-03-04 17:31:43 +0100

    Ready to work, MongoDB is running on Docker Server

--- api/app/config.js
+++ api/app/config.js
@@ -2,11 +2,11 @@ module.exports = {
   development: {
     type: 'development',
     port: 3000,
-    mongodb: ''
+    mongodb: 'mongodb://localhost:27017/cyberdb'
   },
   production: {
     type: 'production',
     port: 3000,
-    mongodb: ''
+    mongodb: 'mongodb://localhost:27017/cyberdb'
   }
-}
+};
--- api/app/models/user.js
+++ api/app/models/user.js
@@ -1,28 +1,31 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const Schema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
+const Schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true
+  {
+    collection: "users",
+    minimize: false,
+    versionKey: false,
   },
-  status: {
-    type: String,
-    required: true
-  }
-}, {
-  collection: 'users',
-  minimize: false,
-  versionKey: false
-}).set('toJSON', {
+).set("toJSON", {
   transform: (doc, ret) => {
-    ret.id = ret._id
+    ret.id = ret._id;
 
-    delete ret._id
-  }
-})
+    delete ret._id;
+  },
+});
 
-module.exports = Schema
+module.exports = Schema;
